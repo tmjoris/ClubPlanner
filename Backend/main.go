ad0b819351e502edc5b38d32b125e6e13f5bd497
tmjoris <tangajoris@gmail.com>
2025-05-02 22:35:59 +0300

    fixed the nil pointer pointer error in db

--- Backend/main.go
+++ Backend/main.go
@@ -35,7 +35,7 @@ func main() {
 
 	//Get a database handle
 	var err error
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
